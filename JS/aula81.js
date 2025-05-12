6da628f05ef33b2b79d8eafc64b8f8579f52b9fc
Augusto Pontes <augustopontes010@gmail.com>
2025-05-11 22:11:49 -0300

    Arrays

---
+++ JS/aula81.js
@@ -0,0 +1,12 @@
+//                  0        1        2
+// const nomes = ['Eduardo', 'Maria', 'Joana'];
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana']
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+// const nome = 'Augusto, Pontes, Cruz, e, Mello,';
+// const nomes = nome.split(', ');
+
+const nomes = ['Augusto', 'Pontes', 'Cruz'];
+const nome = nomes.join(' ');
+console.log(nome);
