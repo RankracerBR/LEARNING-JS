99fba0dd00852185ec4ccbc8232ac65eda489c62
Augusto Pontes <augustopontes010@gmail.com>
2025-09-04 10:38:26 -0300

    Static

---
+++ JS/aula105.js
@@ -0,0 +1,29 @@
+function teste() {
+    console.log(this);
+}
+
+class ControleRemoto{
+    constructor(tv){
+        this.tv = tv;
+        this.volume = 0;
+        teste();
+    }
+
+
+    // Método de instância
+    aumentarVolume() {
+        this.volume += 2;
+    }
+
+    diminuirVolume() {
+        this.volume -= 2;
+    }
+
+    // Método de instância
+    static soma(x, y) {
+        console.log(this);
+    }
+}
+
+const controle1 = new ControleRemoto('LG');
+ControleRemoto.soma();
