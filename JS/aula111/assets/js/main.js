963953dabf0c7fdbd81358f68dcfbe7d06e1c07d
Augusto Pontes <augustopontes010@gmail.com>
2025-09-20 15:25:40 -0300

    Fetch API

--- JS/aula111/assets/js/main.js
+++ JS/aula111/assets/js/main.js
@@ -1,18 +1,18 @@
-const request = obj => {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(obj.method, obj.url, true);
-        xhr.send();
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
 
-        xhr.addEventListener('load', () => {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.responseText);
-            } else {
-                reject(xhr.statusText);
-            }
-        });
-    })
-};
+//         xhr.addEventListener('load', () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText);
+//             } else {
+//                 reject(xhr.statusText);
+//             }
+//         });
+//     })
+// };
 
 
 document.addEventListener('click', e => {
@@ -26,23 +26,29 @@ document.addEventListener('click', e => {
 });
 
 async function carregaPagina(el) {
-    const href = el.getAttribute('href');
-    
-    const objConfig = {
-        method: 'GET',
-        url: href,
-    };
-
     try {
-        const response = await request(objConfig);
-        carregaResultado(response)        
-    } catch(e) {
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+    
+        if(response.status !== 200) throw new Error('ERRO 404!');
+
+        const html = await response.text();
+        carregaResultado(html);
+    } catch(e) { 
         console.log(e);
     }
-
-}
+} 
 
 function carregaResultado(response){
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = response;
 }
+
+
+fetch('pagina1.html')
+    .then(resposta => {
+        if (resposta.status !== 200) throw new Error('ERRO 404 NOSSO');
+        return resposta.text();
+    })
+    .then(html => console.log(html))
+    .catch(e => console.error(e));
